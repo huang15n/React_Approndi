3b4961bbe19734707b8ff2ea073ba3a756b8c006
unknown <huang15n@uwindsor.ca>
2024-12-03 23:25:20 -0500

    composants ajoutee

--- mon-react-application/src/App.jsx
+++ mon-react-application/src/App.jsx
@@ -1,23 +1,28 @@
+import { useState } from "react";
 import { useRef } from "react";
-
 function App() {
-
-  const monRef = useRef();
-
-
-  //  <p>{monRef.current.value}</p> 
-  /***
-   * Le problème avec votre code est que vous tentez d'afficher la valeur de l'élément de formulaire (monRef.current.value) directement dans le JSX sans attendre que l'utilisateur interagisse avec l'élément. Lors du premier rendu du composant, monRef.current est null, car React n'a pas encore lié la référence (ref) à l'élément DOM. Ainsi, accéder à monRef.current.value avant que l'élément ne soit rendu causera une erreur, car la référence n'est pas encore disponible.
-   */
+  const nomRef = useRef();
+  const prenomRef = useRef();
+  const ageRef = useRef();  
+ 
   
+ 
   return (
     <>
-  
-    <input type="text" ref={monRef} />
-    <button onClick={() => alert("Nom soumis: " + monRef.current.value) }>Focus</button>
-     
+
+    <h1> Exemple de useRef </h1>
+    <input type='text' ref={nomRef} />
+    <input type='text' ref={prenomRef} />
+    <input type='text' ref={ageRef} />
+    <button onClick={() => alert("Nom: " + nomRef.current.value + " Prenom: " + prenomRef.current.value + " Age: " + ageRef.current.value)}>Focus</button>
+    
+ 
+ 
     </>
   );
+  
+  
+ 
 }
 
 export default App;
